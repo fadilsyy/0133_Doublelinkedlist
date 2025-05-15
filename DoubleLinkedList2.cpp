78b938b8e16cf415f666080053f5f2f2f759cbf2
fadilsyy <fadilasytar@gmail.com>
2025-05-15 16:13:18 +0700

    penambahan void hapus

--- DoubleLinkedList2.cpp
+++ DoubleLinkedList2.cpp
@@ -79,5 +79,57 @@ class DoubleLinkedList
             if(current->next != NULL)
                 current->next->prev = newNode; // Step 9c: curremt.next.prev = newMode
             current->next = newNode;  // Step 9d: current.next = newNode
+            
         }
+
+        void hapus()
+        {
+            if (START == NULL)
+            {
+                cout << "\nList is empty" << endl;
+                return;
+            }
+
+            cout << "\nMasukkan NIM yang akan didelete";
+            int rollNo;
+            cin >> rollNo;
+
+            Node *current = START;
+
+            // Step 1: Tranverse the list to find the node
+            while(current != NULL && current->noMhs != rollNo)
+            {
+                current = current->next;
+            }
+
+            if (current ==  NULL)
+            {
+                cout << "Record not found" << endl;
+                return;
+            }
+
+            // Step 2: If node is at the beginning
+            if (current == START)
+            {
+                START = current->next; // Step 2a: START = START.next
+                if (START != NULL)
+                {
+                    START->prev = NULL; // Step 2b: START.prev = NULL
+                } 
+            }
+            else
+            {
+                // Step 3: Link previous node to next of current
+                current->prev->next = current->next;
+
+                // Step 4: If current is not the last node
+                if (current->next != NULL)
+                    current->next->prev = current->prev;
+            }
+
+            // Step 5: Delete the node
+            delete current;
+            cout << "Record with roll number" << rollNo << "deleted" << endl;
+        }
+        
 };
